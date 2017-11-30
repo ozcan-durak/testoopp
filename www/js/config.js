1c2350e30c3109aca9820d8f5ee7b12c759a0aca
ozcan durak <info@ozcandurak.com>
2017-11-30 21:45:10 +0000

    icons mobile

--- www/js/config.js
+++ www/js/config.js
@@ -3,6 +3,6 @@
 var krms_driver_config ={
     'ApiUrl':"http://35.188.85.147/~leagrave/driver/api",
     'DialogDefaultTitle':"MKRS",
-    'PushProjectID':"392505290000",
+    'PushProjectID':"",
     'APIHasKey':""
 };
